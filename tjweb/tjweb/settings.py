d6587d380fa63310eef71a461b3c4195379a6d2a
Pornchai <63188163+biggujp@users.noreply.github.com>
2026-03-24 21:02:50 +0700

    update

--- tjweb/tjweb/settings.py
+++ tjweb/tjweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k43$i@=z#46y0cse6jk!9yl&yp691h%jpnym+_#rxe1#uf7apa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
