ceeb8478ed94a572529a46c7e0e2f1b5ba945572
Pornchai <63188163+biggujp@users.noreply.github.com>
2026-04-07 21:30:46 +0700

    update

--- tjweb/myapp/models.py
+++ tjweb/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=20)    
+    email = models.EmailField()
+    point = models.IntegerField(default=1)
+    address = models.CharField(max_length=200, null=True, blank=True)   
+
+    def __str__(self):
+        #return self.name + ' คะแนน: ' + str(self.point) + 'points'  
+        return 'ชื่อ: {} คะแนน: {} points'.format(self.name, self.point)
+
