57526e30f7f60389a69993c9ca84ff2e18b6e7e4
Pornchai <63188163+biggujp@users.noreply.github.com>
2026-06-20 15:37:16 +0700

    add DB of Django

--- BaanSaiJai/myapp/models.py
+++ BaanSaiJai/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    homenum = models.CharField(max_length=6)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    point = models.IntegerField(default=1)
+    address = models.TextField(null=True, blank=True)
+
